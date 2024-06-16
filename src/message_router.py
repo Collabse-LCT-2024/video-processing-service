95d0f379f798cc83596c8307ad17d0c55356c79e
Dmitrii Belik <belik820@gmail.com>
2024-06-16 20:55:16 +0200

    Minor fixes

--- src/message_router.py
+++ src/message_router.py
@@ -23,13 +23,7 @@ class MessageRouter:
             video_id = event.video_id
             video_url = event.video_url
 
-            # video_embedding, video_text, valid = self.audio_processor.process(video_id, video_url)
-
-            video_embedding = None
-
-            video_text = None
-
-            valid = False
+            video_embedding, video_text, valid = self.audio_processor.process(video_id, video_url)
 
             video_properties = EmbeddingData(
                 video_id=video_id,
